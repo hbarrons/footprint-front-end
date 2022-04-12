18c47c9bae1afc37878901b770403f9a935b1932
hbarrons <hbarrons@gmail.com>
2022-04-11 20:35:58 -0400

    add formatting to navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,20 +4,35 @@ const NavBar = ({ user, handleLogout }) => {
   return (
     <>
       {user ?
-        <nav>
-          <ul>
-            <li>Welcome, {user.name}</li>
-            <li><Link to="/footprints/new">Add Footprint</Link></li>
-            <li><Link to="/footprints">My Footprints</Link></li>
-            <li><Link to="/profiles">Profiles</Link></li>
-            <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
-          </ul>
+        <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+          <Link to="/myprofile" class="navbar-brand">my Footprint</Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <div className='container-fluid'>
+              <ul className='navbar-nav'>
+                <li nav-item><Link to="/footprints/new" className='nav-link active'>Add Footprint</Link></li>
+                <li nav-item><Link to="/footprints" className='nav-link active'>My Footprints</Link></li>
+                <li nav-item><Link to="/profiles" className='nav-link active'>Profiles</Link></li>
+                <li nav-item><Link to="" onClick={handleLogout} className='nav-link active'>LOG OUT</Link></li>
+              </ul>
+            </div>
+          </div>
         </nav>
       :
         <nav>
           <ul>
-            <li><Link to="/login">Log In</Link></li>
-            <li><Link to="/signup">Sign Up</Link></li>
+            <li className='nav-item'><Link to="/login" className='nav-link active'>Log In</Link></li>
+            <li className='nav-item'><Link to="/signup" className='nav-link active'>Sign Up</Link></li>
           </ul>
         </nav>
       }
